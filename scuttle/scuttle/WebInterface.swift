2b40a5ea93c6febb6f97d6ffb843719a080cfecd
Harry Nelken <harry.nelken@gmail.com>
2016-02-13 02:43:11 -0500

    added alamofire lib and some example http requests in web interface
    Signed-off-by: Harry Nelken <harry.nelken@gmail.com>

--- scuttle/scuttle/WebInterface.swift
+++ scuttle/scuttle/WebInterface.swift
@@ -7,9 +7,28 @@
 //
 
 import UIKit
+import Alamofire
 
 class WebInterface: NSObject {
     
-    
+    func doStuff() {
+        Alamofire.request(.POST, "http://URLGOESHERE")
+        
+        let parameters = [
+            "foo": [1,2,3],
+            "bar": [
+                "baz": "qux"
+            ]
+        ]
+        
+        Alamofire.request(.POST, "https://httpbin.org/post", parameters: parameters, encoding: .JSON)
+            .validate(statusCode: 200..<300)
+            .validate(contentType: ["application/json"])
+            .response { response in
+                print(response)
+        }
+        
+        
+    }
 
 }
